3792722801949d3fcfc5ba0310b7e040b62d69fb
Dmitry Bashkatov <dbashkatov@gmail.com>
2012-07-29 22:26:55 +0400

    fixes

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LS_CPP=$(subst $(1)/,,$(wildcard $(1)/*.cpp))
 LOCAL_MODULE    := irrlicht_viewer
 LOCAL_SRC_FILES := $(call LS_CPP,$(LOCAL_PATH))
-LOCAL_LDLIBS    := -landroid -llog -lEGL -lGLESv1_CM
+LOCAL_LDLIBS    := -landroid -llog -lEGL
 
 LOCAL_STATIC_LIBRARIES := android_native_app_glue irrlicht
 
